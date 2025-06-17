2b460b59ea3fc0a79b6f648f3e2db1b08dbe1582
vikram somai <vikramsomai4@gmail.com>
2025-06-17 19:48:51 +0530

    added file

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useRef } from "react";
-import "./about.css";
+import "./About.css";
 
 import Header from "../../components/Header/Header";
 import { teamMembers } from "../../data/teamMembers";
